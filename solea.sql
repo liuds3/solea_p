0e808a81036a0ff1a871181d0d447dea1672de13
Karolis <karolispolisciukas0@gmail.com>
2022-05-12 11:57:32 +0300

    answer update
    still bug while deleting answer

--- solea.sql
+++ solea.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2022 at 07:30 PM
+-- Generation Time: May 12, 2022 at 10:56 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -44,7 +44,9 @@ INSERT INTO `answers` (`user`, `question`, `answer`, `id`, `likes`, `dislikes`) 
 ('karpol2', 'Ukraine vs Russia', 'I hope Ukraine wins this war. #SlavaUkraine', 1, 0, 0),
 ('Useris2', 'Pizza or tacos?', 'Pizzas ofc.', 24, 0, 0),
 ('mykolas', 'Luka Doncic in a game 1 loss to the Suns: 45/12/8 ', 'Luka deserves MVPs', 26, 0, 0),
-('kazka', 'Amber vs Depp, who wsins?', 'Justice for Johnny Depp', 27, 0, 0);
+('Useris2', 'Who inspires you to be better?', 'Elon inspires me to study and be better', 42, 1, 0),
+('kazom', 'Who inspires you to be better?', 'Study much more and don\'t be lazy', 44, 0, 0),
+('kazom', 'New question test', 'test answers12', 45, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -59,6 +61,23 @@ CREATE TABLE `liked` (
   `Id` int(11) NOT NULL,
   `likedOrDisliked` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `liked`
+--
+
+INSERT INTO `liked` (`QuestionId`, `AnswerId`, `UserId`, `Id`, `likedOrDisliked`) VALUES
+(0, 41, 164, 1, 1),
+(0, 41, 24, 2, 1),
+(17, 0, 24, 3, 1),
+(16, 0, 24, 4, 1),
+(0, 42, 24, 5, 1),
+(0, 43, 24, 6, 1),
+(15, 0, 24, 7, 1),
+(16, 0, 164, 8, 1),
+(27, 0, 164, 9, 1),
+(0, 45, 164, 10, 1),
+(0, 46, 164, 11, 2);
 
 -- --------------------------------------------------------
 
@@ -94,9 +113,8 @@ INSERT INTO `questions` (`user`, `question`, `id`, `content`, `likes`, `dislikes
 ('karpol2', 'What languages do you speak?', 12, 'Since I am not good with learning languages, so far I can only chat(not talk) English language, Lithuanian, and a bit of Russian language.', 0, 0),
 ('karpol2', 'Ukraine vs Russia', 13, 'Well, I think this discussion is about how thing goes between Ukraine and Russia. I heard that Russia got attacked by Ukraine in one region, but I don\'t remember correctly. But of course, I wish Russia canceled war, or shouldn\'t start war at all. #SlavaUkraini', 0, 0),
 ('kazka', 'Luka Doncic in a game 1 loss to the Suns: 45/12/8 ', 14, 'Not a bad game from Luka, but the suns still pull through and go up 1-0 in the series\r\n\r\n', 0, 0),
-('Useris2', 'What\'s the best piece of advice you ever received?', 15, 'Never give up. Thats the best advice for me, for you, for everyone.', 0, 0),
-('karpol2', 'Who inspires you to be better?', 16, 'Noone. I focus on my own, i don\'t have anyone to inspire, not really much looking forward to inspire from someone.', 0, 0),
-('Useris2', 'About Lithuania', 17, 'Hello guys, is it worth living in Lithuania? What are situation there?', 0, 0);
+('karpol2', 'Who inspires you to be better?', 16, 'Noone. I focus on my own, i don\'t have anyone to inspire, not really much looking forward to inspire from someone.', 2, 0),
+('kazom', 'New question test', 27, 'Content test, asdfaefsafasff', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -117,16 +135,16 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `currency`, `email`, `password`) VALUES
-(24, 'Useris2', 14, 'useris2@gmail.com', 'randomizas'),
+(24, 'Useris2', 18, 'useris2@gmail.com', 'randomizas'),
 (28, 'Useris4', 20, 'useris4@gmail.c', 'passwordz'),
 (111, 'useris1', 300, 'useris1@gmail.c', 'kebabas123'),
 (121, 'mykolas', 200, 'mykolas@gmail.c', 'cesnakinis'),
-(141, 'karpol2', 120, 'karpol2@gmail.c', 'kentauras'),
+(141, 'karpol2', 122, 'karpol2@gmail.c', 'kentauras'),
 (153, 'kazka', 400, 'kazka@gmail.com', 'kentauras'),
 (154, 'testasAI', 275, 'testasAi@gmail.', 'alio'),
 (156, 'test55', 60, 'test55@gmail.co', 'Neatpazyst'),
 (163, 'Testaskarpol2', 300, 'karpolisc@gmail.com', 'ilgasis'),
-(164, 'kazom', 272, 'kazom@gmail.com', 'kazom123');
+(164, 'kazom', 276, 'kazom@gmail.com', 'kazom123');
 
 --
 -- Indexes for dumped tables
@@ -174,13 +192,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -196,14 +214,14 @@ ALTER TABLE `users`
 -- Constraints for table `answers`
 --
 ALTER TABLE `answers`
-  ADD CONSTRAINT `questionai` FOREIGN KEY (`question`) REFERENCES `questions` (`question`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `questionai` FOREIGN KEY (`question`) REFERENCES `questions` (`question`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `userio nickas` FOREIGN KEY (`user`) REFERENCES `users` (`name`);
 
 --
 -- Constraints for table `questions`
 --
 ALTER TABLE `questions`
-  ADD CONSTRAINT `userio` FOREIGN KEY (`user`) REFERENCES `users` (`name`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `userio` FOREIGN KEY (`user`) REFERENCES `users` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
